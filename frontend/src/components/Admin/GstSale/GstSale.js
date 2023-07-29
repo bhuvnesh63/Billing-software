33d467831e9e7c793aad948eb0b4bb5f94f352b1
pooja092 <121798317+pooja092@users.noreply.github.com>
2023-07-29 11:24:35 +0530

    update gstsale js

--- frontend/src/components/Admin/GstSale/GstSale.js
+++ frontend/src/components/Admin/GstSale/GstSale.js
@@ -1,6 +1,6 @@
 import React, { useState } from 'react'
 import Layout from '../../Header/Layout'
-import { Button, Container, Row, Table } from 'react-bootstrap'
+import { Button, Container, Row,Col , Table } from 'react-bootstrap'
 import { Link } from 'react-router-dom'
 import { AiFillDashboard } from 'react-icons/ai';
 import { IoIosCreate } from 'react-icons/io';
@@ -8,21 +8,16 @@ import Form from 'react-bootstrap/Form';
 import "./gstsale.css"
 import ModalCamp from './ModalCamp';
 
-const GstSale = ({post}) => {
+const GstSale = () => {
 
-  const [open, setOpen] = useState(false);
-  const [user, setUser] = useState({});
-  const handleModel = () => {
-    setOpen(true);
-    setUser(post);
-  };
 
-  
+
+
   return (
     <>
 
-    <Layout />
-    <Container style={{ width: '90%', marginTop: '20px' }}>
+      <Layout />
+      <Container style={{ width: '90%', marginTop: '20px' }}>
         <Table striped bordered hover className="main-table">
           <thead>
             <tr>
@@ -48,7 +43,7 @@ const GstSale = ({post}) => {
               </tr>
             </thead>
           </Table>
-       
+
         </Row>
       </Container>
 
@@ -57,48 +52,139 @@ const GstSale = ({post}) => {
         <Container>
           <Row>
 
-          <td>
-              <Button 
-                onClick={handleModel}
-                variant="success"
-                className="float-end"
-              >
-                Create GST
-              </Button>
-              {open && (
-                <ModalCamp
-                  open={open}
-                  setOpen={setOpen}
-                  // selectedItems={selectedItems}
-                  // itemQuantities={itemQuantities}
-                />
-              )}
-            </td>
-
-            {/* <div>
-            <Button
-                onClick={handleModel}
-                variant="success"
-                className="gst-btn float-end" 
-                >
-                Create GST
-              </Button>
-
-            </div> */}
-          
             <form className="row g-4 p-3 registration-form">
 
               <div class="col-md-4 position-relative">
-                <label class="form-label">Customer name</label>
+                <label class="label">Customer name</label>
                 <Form.Select
-              //  value={gender} onChange={(e) => setGender(e.target.value)}
-              //    required
-                 > 
+                //  value={gender} onChange={(e) => setGender(e.target.value)}
+                //    required
+                >
                   <option>Choose</option>
                   <option value="Male">Rr infosoft</option>
-                   <option value="Female">chetu</option>
+                  <option value="Female">chetu</option>
                 </Form.Select>
-              </div> 
+              </div>
+              <div class="col-md-4 position-relative">
+                <label className="label">Name</label>
+                <input
+                  type="text"
+                  class="form-control"
+                // value={service_Name}
+                // onChange={(e) => setService_Name(e.target.value)}
+                // required
+                />
+              </div>
+
+              <div class="col-md-4 position-relative">
+                <label className="label">Phone No</label>
+                <input
+                  type="text"
+                  class="form-control"
+                // value={service_Charge}
+                // onChange={(e) => setService_Charge(e.target.value)}
+                // required
+                />
+              </div>
+
+              <div class="col-md-4 position-relative">
+                <label className="label">Email ID</label>
+                <input
+                  type="text"
+                  class="form-control"
+                // value={service_Charge}
+                // onChange={(e) => setService_Charge(e.target.value)}
+                // required
+                />
+              </div>
+
+              <div class="col-md-4 position-relative">
+                <label className="label">Address</label>
+                <input
+                  type="text"
+                  class="form-control"
+                // value={service_Charge}
+                // onChange={(e) => setService_Charge(e.target.value)}
+                // required
+                />
+              </div>
+
+              <div class="col-md-4 mb-5 position-relative">
+                <label className="label">GST No</label>
+                <input
+                  type="text"
+                  class="form-control"
+                // value={service_Charge}
+                // onChange={(e) => setService_Charge(e.target.value)}
+                // required
+                />
+              </div>
+
+
+              <hr></hr>
+
+              <h5>Product Details</h5>
+
+              <Col sm={6}>
+                <label className="label">Stock </label>
+
+                <Form.Select
+                //  value={gender} onChange={(e) => setGender(e.target.value)}
+                //    required
+                >
+                  <option>Choose</option>
+                  <option value="Male">500</option>
+                  <option value="Female">400</option>
+                  <option value="Female">300</option>
+                  <option value="Female">700</option>
+                  <option value="Female">800</option>
+
+                </Form.Select>
+
+              </Col>
+
+              <Col sm={2}>
+                <label className="label">Price per item </label>
+                <input
+                  type="text"
+                  class="form-control"
+                // value={service_Name}
+                // onChange={(e) => setService_Name(e.target.value)}
+                // required
+                /></Col>
+
+              <Col sm={2}>
+                <label className="label">Quantity </label>
+                <input
+                  type="text"
+                  class="form-control"
+                // value={service_Name}
+                // onChange={(e) => setService_Name(e.target.value)}
+                // required
+                /></Col>
+
+              <Col sm={2}>
+                <label className="label">Total Price </label>
+                <input
+                  type="text"
+                  class="form-control"
+                // value={service_Name}
+                // onChange={(e) => setService_Name(e.target.value)}
+                // required
+                /></Col>
+
+              <div>
+                <Button
+                  className="float-end"
+                  variant="success"
+                  type="submit"
+                // onClick={(event) => submitform(event)} // Pass the event parameter
+                >
+                  Add more
+                </Button>
+
+              </div>
+
 
               <center>
                 <Button
@@ -118,7 +204,7 @@ const GstSale = ({post}) => {
 
 
 
-    
+
     </>
   )
 }
